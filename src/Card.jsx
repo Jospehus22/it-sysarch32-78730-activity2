9f9f98eef2aef2c58124a5e68be9e0cbfae276be
Joseph Manuel Baycat <78730-22220065@SERVER2.DOMAIN>
2024-03-16 12:29:18 +0800

    pusher

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,8 +2,8 @@ function Card(){
     return(
         <div className = "card">
             <img className="card-image" src="https://via.placeholder.com/150" alt = "profile picture"></img>
-            <h3 className="card-title">John Doe</h3>
-            <p className="card-text">John@Doe.com</p>
+            <h3 className="card-title">Joseph Manuel Bagcat</h3>
+            <p className="card-text">bagcatjm@gmail.com</p>
         </div>
     )
 
